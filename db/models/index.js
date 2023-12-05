f10744e9cb43cd79a18d9f214a3b9c5ffef04a5a
Mario Chico <chico.developer@hotmail.com>
2023-12-05 15:40:14 -0600

    Creacion de los modelos para productos y categorias, al igual que su inicializacion. Se corrio una nueva migracion para la creacion de ambas tablas. Tienen una relacion uno a muchos (Una categoria tiene muchos productos)

--- db/models/index.js
+++ db/models/index.js
@@ -1,15 +1,21 @@
 // Se encarga de conectar el ORM con el model, Envia la conexión a los modelos para hacer mapeo y serializacion de datos
 const { User, UserSchema } = require('./user.model') // Esta la configuracion y setup inicial de Sequelize con los modelos, por eso traemos estos
 const { Customer, CustomerSchema} = require('./customers.model');
+const { Category, CategorySchema} = require('./category.model');
+const { Product, ProductSchema} = require('./product.model');
 
 // Le pasamos la conexion
 function setupModels(sequelize){
 
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
 }
 
 // Para correr esto vamos al archivo sequelize en la carpeta libs
